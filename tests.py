8aa541bab9c23ea1b7e7e671aae7ca8d4a550a03
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-12 22:55:33 +0400

    Conditional Comprehension

--- tests.py
+++ tests.py
@@ -16,3 +16,6 @@ print(new_name)
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name for name in names if len(name) < 5]
 print(short_names)
+
+long_names = [name.upper() for name in names if len(name) >= 5]
+print(long_names)
