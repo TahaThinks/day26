1d09e3c2af4d69ea3eddc4f0f56cb0e80ed86718
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-12 22:45:09 +0400

    List Comprehension

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
-# List Comprehension
+# Number List Comprehension
 numbers = [1, 2, 3]
 new_numbers = [n+1 for n in numbers]
 print(new_numbers)
 
+# String List Comprehension
+name = "Angela"
+new_name = [letter for letter in name]
+print(new_name)
