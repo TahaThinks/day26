7f2d21ec8742be3393a25589657988430d7698ae
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-12 23:42:51 +0400

    List of Words

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -1,7 +1,12 @@
-import random
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-students_score = {student:random.randint(1, 100) for student in names}
-print(students_score)
+# import random
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# students_score = {student:random.randint(1, 100) for student in names}
+# print(students_score)
+#
+# passed_students = {student:score for (student,score) in students_score.items() if score>60}
+# print(passed_students)
 
-passed_students = {student:score for (student,score) in students_score.items() if score>60}
-print(passed_students)
+sentence = "What is the Airspeed velocity of an Unladen Swallow?"
+
+result = [word for word in sentence.split()]
+print(result)
