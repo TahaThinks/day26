13fe1b2d66b77d56bb637a81c8190916cafce835
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-12 22:48:56 +0400

    Number List Comprehension using Range

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
-# Number List Comprehension
+# Number List Comprehension 1
 numbers = [1, 2, 3]
 new_numbers = [n+1 for n in numbers]
 print(new_numbers)
+
+# Number List Comprehension 2
+doubled_numbers = [num*2 for num in range(1, 5)]
+print(doubled_numbers)
 
 # String List Comprehension
 name = "Angela"
