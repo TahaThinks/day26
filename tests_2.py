a0ff227c5436666ae832227391db0a9892e9ccac
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-12 23:22:09 +0400

    Create List from File

--- tests_2.py
+++ tests_2.py
@@ -1,12 +1,7 @@
-with open("file1.txt", "r") as file1:
+with open("file1.txt") as file1:
     list1 = file1.readlines()
-
-list1_num = [int(num.strip()) for num in list1]
-# print(list1_num)
-
-with open("file2.txt", "r") as file2:
+with open("file2.txt") as file2:
     list2 = file2.readlines()
 
-list2_num = [int(num.strip()) for num in list2]
-# print(list2_num)
-
+results = [int(num) for num in list1 if num in list2]
+print(results)
