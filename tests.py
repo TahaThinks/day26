8851ab2c40758253e989a367a6179f7ef5859f38
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-12 23:00:19 +0400

    Number List Comprehension 3

--- tests.py
+++ tests.py
@@ -7,15 +7,21 @@ print(new_numbers)
 doubled_numbers = [num*2 for num in range(1, 5)]
 print(doubled_numbers)
 
-# String List Comprehension
+# Number List Comprehension 3
+numbers_2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [number**2 for number in numbers_2]
+print(squared_numbers)
+
+# String List Comprehension 1
 name = "Angela"
 new_name = [letter for letter in name]
 print(new_name)
 
-# Conditional List Comprehension
+# Conditional List Comprehension 1
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name for name in names if len(name) < 5]
 print(short_names)
-
+# Conditional List Comprehension 2
 long_names = [name.upper() for name in names if len(name) >= 5]
 print(long_names)
+
