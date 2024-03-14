98e80b819f03e98b1e28c26b79a6613aa2d97a7d
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-15 00:14:09 +0400

    Get Input

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas
 
+# Create a DataFrame from a csv file:
 nato_phonetic_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 # print(nato_phonetic_data_frame)
 
-nato_phonetic_dict = {row.letter:row.code for (index,row) in nato_phonetic_data_frame.iterrows()}
-print(nato_phonetic_dict)
+# Create a dictionary from DataFrame:
+nato_phonetic_dict = {row.letter: row.code for (index, row) in nato_phonetic_data_frame.iterrows()}
+# print(nato_phonetic_dict)
 
+word = input("Enter a Word: ").upper()
+# print(word)
