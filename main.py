bc1e671d3b3d3a822e57eff07c465d8a6860e592
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-15 00:11:17 +0400

    Convert DataFrame to Dict

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import pandas
 
 nato_phonetic_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 # print(nato_phonetic_data_frame)
+
+nato_phonetic_dict = {row.letter:row.code for (index,row) in nato_phonetic_data_frame.iterrows()}
+print(nato_phonetic_dict)
+
