246dc5ae3c3794c476996f492994bce9c50da88c
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-14 23:55:19 +0400

    Iterrows

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ print(student_data_frame)
 # Loop through a Data Frame
 for (key, value) in student_data_frame.items():
     print(value)
+
+# Loop through rows of a data frame
+
+for (index, row) in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
