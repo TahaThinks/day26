9559988bd2333205954e8e3f8599a80ef0376c7a
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-15 00:00:03 +0400

    New

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -20,3 +20,22 @@
 # # Looping through dictionaries
 # for (key, value) in student_dict.items():
 #     print(key)
+
+
+# student_dict = {
+#     "student": ["Angela", "James", "Lilly"],
+#     "score": [56, 76, 98]
+# }
+#
+# student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
+#
+# # # Loop through a Data Frame
+# # for (key, value) in student_data_frame.items():
+# #     print(value)
+#
+# # Loop through rows of a data frame
+#
+# for (index, row) in student_data_frame.iterrows():
+#     if row.student == "Angela":
+#         print(row.score)
--- main.py
+++ main.py
@@ -1,19 +1,2 @@
 import pandas
 
-student_dict = {
-    "student": ["Angela", "James", "Lilly"],
-    "score": [56, 76, 98]
-}
-
-student_data_frame = pandas.DataFrame(student_dict)
-print(student_data_frame)
-
-# Loop through a Data Frame
-for (key, value) in student_data_frame.items():
-    print(value)
-
-# Loop through rows of a data frame
-
-for (index, row) in student_data_frame.iterrows():
-    if row.student == "Angela":
-        print(row.score)
