b6486c8f560fbfed2a7ed9692d5591f7e02d75d1
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-14 23:46:09 +0400

    Looping though Dict

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -5,10 +5,18 @@
 #
 # passed_students = {student:score for (student,score) in students_score.items() if score>60}
 # print(passed_students)
+#
+# sentence = "What is the Airspeed velocity of an Unladen Swallow?"
+#
+# words_list = [word for word in sentence.split()]
+# print(words_list)
+# words_dict = {word:len(word) for word in words_list}
+# print(words_dict)
 
-sentence = "What is the Airspeed velocity of an Unladen Swallow?"
-
-words_list = [word for word in sentence.split()]
-print(words_list)
-words_dict = {word:len(word) for word in words_list}
-print(words_dict)
+# student_dict = {
+#     "student": ["Angela", "James", "Lilly"],
+#     "score": [56, 76, 98]
+# }
+# # Looping through dictionaries
+# for (key, value) in student_dict.items():
+#     print(key)
